430080ae8a2dfbfe39b452e197b0aef82bb19d69
BigBetaTester <weimerh@hotmail.com>
2018-10-27 20:51:51 -0400

    Sleep 5ms when not fullscreen.  Removed the sleep 1ms that was slowing mouse when navigating menus in fullscreen.

--- src/WinTrek/WinTrek.cpp
+++ src/WinTrek/WinTrek.cpp
@@ -7723,8 +7723,13 @@ public:
             //------------------------------------------------------------------------------
             // End interception for training missions
             //------------------------------------------------------------------------------
-		}
 
+			} else {
+			//-Imago 7/13/09 we're not actually in a sector playing the game...
+			// this is the right time & place place to rest our CPU. We can also give it more of a break now.
+			if (!GetFullscreen())
+			Sleep(5);
+		}
         //
         // Handle sending network messages
         //
