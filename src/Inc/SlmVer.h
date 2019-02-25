9150348bc54915ba9fb3ea199ffb1453630b7548
BigBetaTester <8833847+BigBetaTester@users.noreply.github.com>
2019-02-24 19:54:02 -0500

    Revert "Re-added missing DRM stage to clintlib"
    This reverts commit 6b1acf4fbb6f3aa8457421a3496764165a751251.

--- src/Inc/SlmVer.h
+++ src/Inc/SlmVer.h
@@ -1,7 +1,7 @@
 // mmf modified version, these set FileVersion and ProductVersion used by VerRes.rc
 #define rmj		1
 #define rmm		1
-#define rel     1000006.00       // mmf added to designate release number for a given build
-#define rup		230114  // mmf make sure this has 6 digits otherwise version number conversion for esc display may fail
+#define rel     1000005.00       // mmf added to designate release number for a given build
+#define rup		221333  // mmf make sure this has 6 digits otherwise version number conversion for esc display may fail
 #define szVerName	""
 #define szVerUser	"CURTC3"
