a1faa344f2d3fa2967494eae937dca4b78277435
BackTrak <nick@zaphop.com>
2026-01-28 17:50:36 -0600

    feat: fixing busted AGC build!

--- src/AGC/Pre-build.bat
+++ src/AGC/Pre-build.bat
@@ -1,5 +1,10 @@
-cscript /e:jscript /NoLogo ..\src\AGC\XMLXForm.js ..\src\AGC\AGCEvents.xml ..\src\AGC\AGCEventsCPP.xsl %1AGCEventsCPP.h
-cscript /e:jscript /NoLogo ..\src\AGC\XMLXForm.js ..\src\AGC\AGCEvents.xml ..\src\AGC\AGCEventsRC2.xsl %1AGCEventsRC2.rc2
-cscript /e:jscript /NoLogo ..\src\AGC\XMLXForm.js ..\src\AGC\AGCEvents.xml ..\src\AGC\AGCEventsRCH.xsl %1AGCEventsRCH.h
-cscript /e:jscript /NoLogo ..\src\AGC\XMLXForm.js ..\src\AGC\AGCEvents.xml ..\src\AGC\AGCEventsMC.xsl %1AGCEventsMC.mc
-mc.exe -r %1 -h %1 %1AGCEventsMC.mc
+echo Building message resources for AGCEvents...
+set targetDir=..\%1
+cscript /e:jscript /NoLogo ..\..\src\AGC\XMLXForm.js ..\..\src\AGC\AGCEvents.xml ..\..\src\AGC\AGCEventsCPP.xsl %targetDir%AGCEventsCPP.h
+cscript /e:jscript /NoLogo ..\..\src\AGC\XMLXForm.js ..\..\src\AGC\AGCEvents.xml ..\..\src\AGC\AGCEventsRC2.xsl %targetDir%AGCEventsRC2.rc2
+cscript /e:jscript /NoLogo ..\..\src\AGC\XMLXForm.js ..\..\src\AGC\AGCEvents.xml ..\..\src\AGC\AGCEventsRCH.xsl %targetDir%AGCEventsRCH.h
+cscript /e:jscript /NoLogo ..\..\src\AGC\XMLXForm.js ..\..\src\AGC\AGCEvents.xml ..\..\src\AGC\AGCEventsMC.xsl %targetDir%AGCEventsMC.mc
+
+echo Compiling AGC messages
+echo mc.exe -r %targetDir% -h %targetDir% %targetDir%AGCEventsMC.mc
+mc.exe -r %targetDir% -h %targetDir% %targetDir%AGCEventsMC.mc
